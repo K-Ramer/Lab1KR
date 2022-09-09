2615b9f40137f02afa2e297beeb5afda451c1242
katarinaramer <katarina.ramer@iths.se>
2022-09-09 11:14:56 +0200

    Meny tillagd

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿
+﻿string[] APImeny = { "Bullar", "Kakor", "Kaffe" };
+
 Console.WriteLine("Välkommen till Ninas bageri!");
+
+string meny = "";
+for (int fika = 0; fika < APImeny.Length; fika++)
+{
+    if (fika == APImeny.Length - 1)
+        meny += "och ";
+
+    meny += APImeny[fika] + " ";
+}
+
+Console.WriteLine("Här finns " + meny);
 
